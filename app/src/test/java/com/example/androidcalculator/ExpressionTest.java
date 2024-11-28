f5a333e22d151861344d12e148f14a8610f6bf63
Kot Andrii <andrew4kot@gmail.com>
2024-11-28 22:09:10 +0200

    Finished Member class

---
+++ app/src/test/java/com/example/androidcalculator/ExpressionTest.java
@@ -0,0 +1,15 @@
+package com.example.androidcalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.widget.Button;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void testOnButtonClick() {
+    }
+}
